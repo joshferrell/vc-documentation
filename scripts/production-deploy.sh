4b152d6ca7d5882c30bda9be69d324960422073b
Josh Ferrell <josh@joshferrell.me>
2018-06-24 17:30:21 -0700

    feat: added mongodb to sloppy deploy script

--- scripts/production-deploy.sh
+++ scripts/production-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./sloppy change -v docker_image:"$DOCKER_REPOSITORY/$DOCKER_IMAGE:$TRAVIS_COMMIT" -v domain:"$DOMAIN" ./sloppy.json
+./sloppy change -v docker_image:"$DOCKER_REPOSITORY/$DOCKER_IMAGE:$TRAVIS_COMMIT" -v domain:"$DOMAIN" -v db:"$MONGODB_CONNECTION_STRING" ./sloppy.json
